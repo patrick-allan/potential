4209687aa54c9a1112c14cca3f825d9c0ccd5911
patrick-allan <prkallan1337@gmail.com>
2021-08-18 11:56:52 -0300

    developer controller v1 - todas funções

--- api/app/Http/Controllers/DevelopersController.php
+++ api/app/Http/Controllers/DevelopersController.php
@@ -3,38 +3,102 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Response;
 
 use App\Models\Developers;
 
 class DevelopersController extends Controller
 {
-    public function __construct()
-    {
-    }
-
     public function index(Request $request, $id = null)
     {
-        //dd($request, $id);
-        //$result = DB::table('developers')->get();
-        $developers = Developers::all();
-        return $developers->toJson();
+        $developers = new Developers;
+        if (!is_null($id)) {
+            return $developers::findOrFail($id);
+        } elseif (empty($request->query())) {
+            return $developers::all();
+        } else {
+            $validated = $this->validate($request, [
+                'nome'           => 'string|max:200|regex:/^[a-zA-Z\s]*$/',
+                'sexo'           => 'string|max:1|in:F,M',
+                'hobby'          => 'max:200',
+                'datanascimento' => 'date',
+                'idade'          => 'integer|min:1'
+            ]);          
+            $conditions = [];
+            if (isset($validated['id'])) {
+                array_push($conditions, ['id', $validated['id']]);
+            }
+
+            if (isset($validated['nome'])) {
+                array_push($conditions, ['nome','like','%'.$validated['nome'].'%']);
+            }
+
+            if (isset($validated['sexo'])) {
+                array_push($conditions, ['sexo', $validated['sexo']]);
+            }
+
+            if (isset($validated['hobby'])) {
+                array_push($conditions, ['hobby','like','%'.$validated['hobby'].'%']);
+            }
+
+            if (isset($validated['datanascimento'])) {
+                array_push($conditions, ['datanascimento', $validated['datanascimento']]);
+            }
+
+            if (isset($validated['idade'])) {
+                array_push($conditions, ['idade', $validated['idade']]);
+            }            
+            //dd($conditions);
+            $result = Developers::where($conditions)->paginate(2);
+            return $result;
+        }
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        return json_encode(['post'=>'Adiciona um novo desenvolvedor']);
+        $validated = $this->validate($request, [
+            'nome'           => 'required|string|max:200|regex:/^[a-zA-Z\s]*$/',
+            'sexo'           => 'required|string|max:1|in:F,M',
+            'hobby'          => 'nullable|max:200',
+            'datanascimento' => 'required|date',
+        ]);
+        try {
+            $validated['idade'] = date_diff(date_create(), date_create($validated['datanascimento']))->y;
+            Developers::create(array_map('trim', $validated));
+            return response()->json(['success' => 'desenvolvedor cadastrado'], 201);
+        } catch (\Exception $e) {
+            abort(400);
+        }
     }
 
-    public function update($id)
-    {        
-        $result = ['put'=>'Atualiza os dados de um desenvolvedor', 'id' => $id];
-        return $result;
+    public function update(Request $request, $id)
+    {
+        $validated = $this->validate($request, [
+            'nome'           => 'string|max:200|regex:/^[a-zA-Z\s]*$/',
+            'sexo'           => 'string|max:1|in:F,M',
+            'hobby'          => 'nullable|max:200',
+            'datanascimento' => 'date',
+        ]);
+        try {
+            if (isset($validated['datanascimento'])) {
+                $validated['idade'] = date_diff(date_create(), date_create($validated['datanascimento']))->y;
+            }
+            $developer = new Developers;
+            $developer::where('id', $id)->update(array_map('trim', $validated));
+            return response()->json(['success' => 'desenvolvedor atualizado'], 200);
+        } catch (\Exception $e) {
+            abort(400);
+        }
     }
 
     public function delete($id)
     {
-        $result = ['delete'=>'Apaga o registro de um desenvolvedor', 'id' => $id];
-        return $result;
+        try {
+            $developer = Developers::findOrFail($id);
+            $developer->delete();
+            return response()->json([], 204);
+        } catch (\Exception $e) {
+            abort(400);
+        }
     }
 }
