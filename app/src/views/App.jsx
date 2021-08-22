4cc48c49e5628e137c0e4b163e5822a1bec2f3d4
patrick-allan <prkallan1337@gmail.com>
2021-08-22 14:41:58 -0300

    docker test

--- app/src/views/App.jsx
+++ app/src/views/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Menu from '../components/layout/Menu';
 import Content from '../components/layout/Content';
+import './App.css';
 
 const App = (props) => (
     <div className="App">        
